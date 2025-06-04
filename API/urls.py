4c37ad8decc780f3d6125637649226e871d10eff
Marlonn Locatelli <marlonn@gmail.com>
2025-06-04 17:28:06 -0300

    ✨rest api apupo_detail_view

--- API/urls.py
+++ API/urls.py
@@ -18,6 +18,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from apupos.views import home_view, apupo_detail_view
+
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", home_view),
+    path("apupo/<int:apupo_id>", apupo_detail_view),
 ]
--- apupos/views.py
+++ apupos/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
+from django.http import HttpResponse, Http404, JsonResponse
 
-# Create your views here.
+from .models import Apupo
+
+
+def home_view(request, *args, **kwargs):
+    return HttpResponse("<p>Hello</p>")
+
+
+def apupo_detail_view(request, apupo_id, *args, **kwargs):
+    """
+    REST API VIEW
+    returning JSON data
+    """
+
+    data = {
+        "id": apupo_id,
+        # "image_path": obj.image.url
+    }
+    status = 200
+
+    try:
+        obj = Apupo.objects.get(id=apupo_id)
+        data["content"] = obj.content
+    except Exception as e:
+        data["message"] = f"Apupo with id {apupo_id} was not found"
+        status = 404
+        # worked with HttpResponse
+        # raise Http404
+
+    return JsonResponse(data, status=status)
+    # returning a page
+    # return HttpResponse(f"<h1>Detail of id {apupo_id}</h1><p>{obj.content}</p>")
