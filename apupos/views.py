fdc496717e119fa21f2a6f67809de979625e1a69
Marlonn Locatelli <marlonn@gmail.com>
2025-06-05 15:14:28 -0300

    ✨add likes to rest api

--- apupos/views.py
+++ apupos/views.py
@@ -14,7 +14,7 @@ def apupo_list_view(request, *args, **kwargs):
     REST API VIEW
     """
     queryset = Apupo.objects.all()
-    apupos_list = [{"id": i.id, "content": i.content} for i in queryset]
+    apupos_list = [{"id": i.id, "content": i.content, "likes": 12} for i in queryset]
     data = {"response": apupos_list}
 
     return JsonResponse(data)
