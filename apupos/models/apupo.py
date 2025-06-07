e74cd952ba80629db77c15491b311fc8646633b0
Marlonn Locatelli <marlonn@gmail.com>
2025-06-07 15:18:26 -0300

    🔨update apupo model ordering and add serialize method

--- apupos/models/apupo.py
+++ apupos/models/apupo.py
@@ -5,3 +5,12 @@ class Apupo(models.Model):
     content = models.TextField(blank=True, null=True)
     image = models.FileField(upload_to="images/", blank=True)
     
+    class Meta:
+        ordering = ["-id"] # ordem descendente
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "content": self.content,
+            "likes": 10
+        }
