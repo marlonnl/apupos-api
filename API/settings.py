71e02b81218a0a94da7bbaac04da6a4914fab936
Marlonn Locatelli <marlonn@gmail.com>
2025-06-08 18:04:12 -0300

    💄 add global variable MAX_POST_LENGTH to settings

--- API/settings.py
+++ API/settings.py
@@ -29,6 +29,8 @@ DEBUG = True
 ALLOWED_HOSTS = ["127.0.0.1"]
 LOGIN_URL = "/login"
 
+MAX_POST_LENGTH = 140
+
 # Application definition
 
 INSTALLED_APPS = [
@@ -38,6 +40,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
     "apupos",
 ]
 
