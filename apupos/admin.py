a01fad2d259fded816fb801b41294e4092b74804
Marlonn Locatelli <marlonn@gmail.com>
2025-06-08 17:02:33 -0300

    🆕setting up admin and model admin

--- apupos/admin.py
+++ apupos/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models.apupo import Apupo
+
+
+class ApupoAdmin(admin.ModelAdmin):
+    list_display = ["__str__", "user"]
+    search_fields = ["user__username", "user__email"]
+
+    class Meta:
+        model = Apupo
+
+
+admin.site.register(Apupo, ApupoAdmin)
--- apupos/models/apupo.py
+++ apupos/models/apupo.py
@@ -1,16 +1,20 @@
 from django.db import models
 
+# user
+from django.conf import settings
+
+
 class Apupo(models.Model):
     # id = models.AutoField(primary_key=True)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     content = models.TextField(blank=True, null=True)
     image = models.FileField(upload_to="images/", blank=True)
-    
+
     class Meta:
-        ordering = ["-id"] # ordem descendente
+        ordering = ["-id"]  # ordem descendente
+
+    def __str__(self):
+        return self.content
 
     def serialize(self):
-        return {
-            "id": self.id,
-            "content": self.content,
-            "likes": 10
-        }
+        return {"id": self.id, "content": self.content, "likes": 10}
