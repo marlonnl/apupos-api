59f38ed44280c4ec9c53d4efa9730c519482782a
Marlonn Locatelli <marlonn@gmail.com>
2025-06-09 15:49:59 -0300

    ✅ add authentication classes and permission_classes to create_view

--- apupos/views/apupo_create_view.py
+++ apupos/views/apupo_create_view.py
@@ -1,13 +1,17 @@
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes, authentication_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.authentication import SessionAuthentication
 
 from ..serializers.apupo_serializer import ApupoSerializer
 
 
-@api_view(["POST"]) # only POST method
+@api_view(["POST"])  # only POST method
+@authentication_classes([SessionAuthentication])
+@permission_classes([IsAuthenticated])  # se estiver autenticado, terá acesso a função
 def apupo_create_view(request, *args, **kwargs):
     serializer = ApupoSerializer(data=request.POST)
-    
+
     if serializer.is_valid(raise_exception=True):
         serializer.save(user=request.user)  # content=request.content
         return Response(serializer.data, status=201)
