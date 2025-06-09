29472c7df59aa03d69615015117bdcbf27c26303
Marlonn Locatelli <marlonn@gmail.com>
2025-06-09 17:30:44 -0300

    ✨ adding likes to admin

--- apupos/admin.py
+++ apupos/admin.py
@@ -2,9 +2,15 @@ from django.contrib import admin
 
 # Register your models here.
 from .models.apupo import Apupo
+from .models.apupo_like import ApupoLike
+
+
+class ApupoLikeAdmin(admin.TabularInline):
+    model = ApupoLike
 
 
 class ApupoAdmin(admin.ModelAdmin):
+    inlines = [ApupoLikeAdmin]
     list_display = ["__str__", "user"]
     search_fields = ["user__username", "user__email"]
 
