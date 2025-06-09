ef66b5cdb6f0275bc4991c0fcf7adb2741c569e5
Marlonn Locatelli <marlonn@gmail.com>
2025-06-09 17:19:15 -0300

    🎨 "User" variable defnied

--- apupos/models/apupo.py
+++ apupos/models/apupo.py
@@ -4,9 +4,12 @@ from django.db import models
 from django.conf import settings
 
 
+User = settings.AUTH_USER_MODEL
+
+
 class Apupo(models.Model):
     # id = models.AutoField(primary_key=True)
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.TextField(blank=True, null=True)
     image = models.FileField(upload_to="images/", blank=True)
 
