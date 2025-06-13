4a53b32e26dab3d8d60ad94ad758e9e6752a9b42
Marlonn Locatelli <marlonn@gmail.com>
2025-06-13 15:18:55 -0300

    ✨ serializers handling rt

--- apupos/serializers/apupo_action_serializer.py
+++ apupos/serializers/apupo_action_serializer.py
@@ -5,6 +5,7 @@ from rest_framework import serializers
 class ApupoActionSerializer(serializers.Serializer):
     id = serializers.IntegerField()
     action = serializers.CharField()
+    content = serializers.CharField(allow_blank=True, required=False)
 
     def validate_action(self, value):
         if value not in settings.ACTION_OPTIONS:
--- apupos/serializers/apupo_serializer.py
+++ apupos/serializers/apupo_serializer.py
@@ -2,18 +2,25 @@ from django.conf import settings
 from rest_framework import serializers
 
 from ..models.apupo import Apupo
+from ..serializers import ApupoCreateSerializer
 
 
 class ApupoSerializer(serializers.ModelSerializer):
     likes = serializers.SerializerMethodField(read_only=True)
-    content = serializers.SerializerMethodField(read_only=True)
+    # content = serializers.SerializerMethodField(read_only=True)
+    parent = ApupoCreateSerializer(read_only=True)
 
     class Meta:
         model = Apupo
-        fields = ["id", "content", "likes"]
+        fields = ["id", "content", "likes", "is_retweet", "parent"]
 
     def get_likes(self, obj):
         return obj.likes.count()
 
     def get_content(self, obj):
-        return obj.content
+        content = obj.content
+
+        if obj.is_retweet:
+            content = obj.parent.content
+
+        return content
