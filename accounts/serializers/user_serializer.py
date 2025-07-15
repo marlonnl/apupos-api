2d7085efebaa34d02dc66225dccf70b73422160c
Marlonn Locatelli <marlonn@gmail.com>
2025-07-15 17:23:08 -0300

    ✨ login, logout and registration serializers

---
+++ accounts/serializers/user_serializer.py
@@ -0,0 +1,46 @@
+from django.contrib.auth import get_user_model, authenticate
+from rest_framework import serializers
+
+
+User = get_user_model()
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ("id", "username", "email", "first_name")
+
+
+class RegistrationSerializer(serializers.ModelSerializer):
+    password1 = serializers.CharField(write_only=True)
+    password2 = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ("username", "email", "password1", "password2")
+        extra_kwargs = {"password": {"write_only": True}}
+
+    def validate(self, attrs):
+        if attrs["password1"] != attrs["password2"]:
+            raise serializers.ValidationError("Passwords do not match")
+
+        return attrs
+
+    def create(self, validated_data):
+        password = validated_data.pop("password1")
+        validated_data.pop("password2")
+
+        return User.objects.create_user(password=password, **validated_data)
+
+
+class UserLoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField(write_only=True)
+
+    def validate(self, data):
+        user = authenticate(**data)
+
+        if user and user.is_active:
+            return user
+
+        raise serializers.ValidationError("Login incorrect")
