c06ca443d077037a116742b7ee217567ada672d5
Marlonn Locatelli <marlonn@gmail.com>
2025-07-30 18:07:07 -0300

    ✨ custom JWT auth (using cookies)

--- accounts/authentication.py
+++ accounts/authentication.py
@@ -3,12 +3,15 @@ from rest_framework_simplejwt.authentication import JWTAuthentication
 
 class CookiesJWTAuthentication(JWTAuthentication):
     def authenticate(self, request):
-        access_token = request.COOKIES.get("access_token")
+        access_token = request.COOKIES.get("accessToken")
+        # print("access:", access_token)
+        # print(request.body)
 
         if not access_token:
             return None
 
         validated_token = self.get_validated_token(access_token)
+        # print("validated:", validated_token)
 
         try:
             user = self.get_user(validated_token)
@@ -16,4 +19,5 @@ class CookiesJWTAuthentication(JWTAuthentication):
         except Exception as e:
             return None
 
+        # print(user, access_token)
         return (user, access_token)
