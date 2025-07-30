270482caef200dd08b99cb26fab838c05a50c820
Marlonn Locatelli <marlonn@gmail.com>
2025-07-30 20:23:36 -0300

    🧹 removed comments, cleaning code

--- API/urls.py
+++ API/urls.py
@@ -1,15 +1,9 @@
 from django.contrib import admin
-from django.urls import path, re_path, include
-
-# from rest_framework_simplejwt import TokenObtainPairView, TokenRefreshView
+from django.urls import path, include
 
 from apupos.views import (
     home_view,
-    apupo_detail_view,
     apupo_list_view,
-    apupo_create_view,
-    apupo_delete_view,
-    apupo_action_view,
 )
 
 urlpatterns = [
@@ -19,8 +13,4 @@ urlpatterns = [
     path("api/apupo/", include("apupos.urls")),
     path("api/auth/", include("accounts.urls")),
     path("api/profile/", include("profiles.urls")),
-    # path("create-apupo", apupo_create_view),
-    # path("apupo/<int:apupo_id>", apupo_detail_view),
-    # path("api/apupo/<int:apupo_id>/delete", apupo_delete_view),
-    # path("api/apupo/action", apupo_action_view),
 ]
