4b5d709f576e36b9bff56ef2e0b94594deb8cd18
Marlonn Locatelli <marlonn@gmail.com>
2025-07-30 21:10:04 -0300

    ✨ profile view REST API

--- profiles/views/profile_detail_view.py
+++ profiles/views/profile_detail_view.py
@@ -1,5 +1,17 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
+from ..models.profile import Profile
+from ..serializers.profile_serializer import ProfileSerializer
 
 
-def profile_detail_view(request, *args, **kwargs):
-    return render(request)
+@api_view(["GET"])
+def profile_detail_view(request, username, *args, **kwargs):
+    queryset = Profile.objects.filter(user__username=username)
+
+    if not queryset:
+        return Response({"profile": "perfil não existe."}, status=404)
+
+    serializer = ProfileSerializer(queryset, many=True)
+
+    return Response(serializer.data)
