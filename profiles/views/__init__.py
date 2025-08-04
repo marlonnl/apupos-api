f61900290424662fde8e6a986e0e1c845d2c93a5
Marlonn Locatelli <marlonn@gmail.com>
2025-08-04 14:33:28 -0300

    ✨ added edit/update profile view

--- profiles/views/__init__.py
+++ profiles/views/__init__.py
@@ -1,1 +1,2 @@
 from .profile_detail_view import profile_detail_view
+from .profile_update_view import profile_update_view
---
+++ profiles/views/profile_update_view.py
@@ -0,0 +1,29 @@
+from rest_framework.response import Response
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+
+from profiles.serializers.profile_serializer import ProfileSerializer
+
+from ..models.profile import Profile
+
+
+@api_view(["POST"])
+@permission_classes([IsAuthenticated])
+def profile_update_view(request, *args, **kwargs):
+    # print(request.data)
+
+    qs = Profile.objects.filter(user__username=request.user).first()
+
+    if not qs:
+        return Response({"profile": "perfil não existe"}, status=404)
+
+    # fields = ["name", "bio", "site", "location"]
+
+    qs.name = request.data["name"]
+    qs.bio = request.data["bio"]
+    qs.site = request.data["site"]
+    qs.location = request.data["location"]
+
+    qs.save()
+
+    return Response({"profile": "updated"}, status=201)
