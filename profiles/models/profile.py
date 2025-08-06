c8b7de543059ece329a686c1668774793a4a1eb0
Marlonn Locatelli <marlonn@gmail.com>
2025-08-06 15:12:31 -0300

    ✨ added followers to Profile model

--- profiles/models/profile.py
+++ profiles/models/profile.py
@@ -8,10 +8,16 @@ User = settings.AUTH_USER_MODEL
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+
     name = models.CharField(max_length=80, null=True, blank=True)
     bio = models.CharField(max_length=400, null=True, blank=True)
     site = models.URLField(null=True, blank=True)
     location = models.CharField(max_length=200, null=True, blank=True)
+
+    timestamp = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    followers = models.ManyToManyField(User, related_name="following", blank=True)
 
     def __str__(self):
         return f"{self.user}"
