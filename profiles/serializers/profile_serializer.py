e667f79465948bd6df4609a44f3b8e5c9207ac39
Marlonn Locatelli <marlonn@gmail.com>
2025-08-07 19:16:48 -0300

    ✨ added follow numbers to Profile serializer

--- profiles/serializers/profile_serializer.py
+++ profiles/serializers/profile_serializer.py
@@ -4,11 +4,44 @@ from ..models.profile import Profile
 
 
 class ProfileSerializer(serializers.ModelSerializer):
-    username = serializers.CharField(source="user.username", read_only=True)
+    username = serializers.SerializerMethodField(read_only=True)
+    first_name = serializers.SerializerMethodField(read_only=True)
+
+    following_count = serializers.SerializerMethodField(read_only=True)
+    followers_count = serializers.SerializerMethodField(read_only=True)
 
     class Meta:
         model = Profile
-        fields = ["id", "user", "username", "name", "bio", "site", "location"]
+        fields = [
+            "id",
+            "username",
+            "first_name",
+            "bio",
+            "site",
+            "location",
+            "following_count",
+            "followers_count",
+        ]
+
+    def get_first_name(self, obj):
+        return obj.user.first_name
+
+    def get_username(self, obj):
+        return obj.user.username
+
+    def get_following_count(self, obj):
+        return obj.user.following.count()
+
+    def get_followers_count(self, obj):
+        return obj.followers.count()
+
+
+# class ProfileSerializer(serializers.ModelSerializer):
+#     username = serializers.CharField(source="user.username", read_only=True)
+
+#     class Meta:
+#         model = Profile
+#         fields = ["id", "user", "username", "name", "bio", "site", "location"]
 
 
 class ProfileSerializerLogin(serializers.ModelSerializer):
@@ -17,3 +50,8 @@ class ProfileSerializerLogin(serializers.ModelSerializer):
     class Meta:
         model = Profile
         fields = ["id", "username", "name", "bio", "site", "location"]
+
+
+class FollowingSerializer(serializers.ModelField):
+    class Meta:
+        model = Profile
