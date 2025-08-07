591d1b3f243fdce3aa12c3a25da1a48f6f6acecc
Marlonn Locatelli <marlonn@gmail.com>
2025-08-07 19:17:26 -0300

    ✨ added pagination and comments on functions

--- apupos/views/apupo_list_view.py
+++ apupos/views/apupo_list_view.py
@@ -1,25 +1,56 @@
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.pagination import PageNumberPagination
 
 from ..models.apupo import Apupo
 from ..serializers.apupo_serializer import ApupoSerializer
 
 
-@api_view(["GET"])  # only GET method
+PAGE_SIZE = 10
+
+
+def get_paginated_qs_response(qs, request):
+    paginator = PageNumberPagination()
+    paginator.page_size = PAGE_SIZE
+
+    paginated_queryset = paginator.paginate_queryset(qs, request)
+    serializer = ApupoSerializer(paginated_queryset, many=True)
+
+    return paginator.get_paginated_response(serializer.data)
+
+
+@api_view(["GET"])
 def apupo_list_view(request, *args, **kwargs):
     """
-    REST API VIEW
+    Returns all posts.
+    If a "username" is not provided, it returns all posts.
     """
 
     queryset = Apupo.objects.all()
 
     username = request.GET.get("username")  # url.. ?username={username}
     if username is not None:
-        queryset = queryset.filter(user__username__iexact=username)
+        queryset = queryset.by_username(username)
 
-    serializer = ApupoSerializer(
-        queryset, many=True
-    )  # many=True means the queryset has multiple items
+    # serializer = ApupoSerializer(queryset, many=True)
 
     # print(serializer.data)
-    return Response(serializer.data)
+    # return Response(serializer.data, status=200)
+    return get_paginated_qs_response(queryset, request)
+
+
+@api_view(["GET"])
+@permission_classes([IsAuthenticated])
+def apupo_feed_view(request, *args, **kwargs):
+    """
+    Returns all posts from the authenticated user.
+    """
+
+    user = request.user
+
+    queryset = Apupo.objects.feed(user)
+    # serializer = ApupoSerializer(queryset, many=True)
+
+    # return Response(serializer.data, status=200)
+    return get_paginated_qs_response(queryset, request)
