3b1e79bbf3557f5554b4654960657529b830412b
Marlonn Locatelli <marlonn@gmail.com>
2025-08-07 19:15:57 -0300

    ✨ added better user field (and follow numbers)

--- apupos/serializers/apupo_create_serializer.py
+++ apupos/serializers/apupo_create_serializer.py
@@ -1,20 +1,23 @@
 from django.conf import settings
 from rest_framework import serializers
 
+from profiles.serializers.profile_serializer import ProfileSerializer
+
 from ..models.apupo import Apupo
 
 
 class ApupoCreateSerializer(serializers.ModelSerializer):
+    user = ProfileSerializer(source="user.profile", read_only=True)
     likes = serializers.SerializerMethodField(read_only=True)
 
     class Meta:
         model = Apupo
-        fields = ["id", "content", "likes"]
+        fields = ["id", "user", "content", "likes"]
 
     def get_likes(self, obj):
         return obj.likes.count()
 
     def validate_content(self, value):
         if len(value) > settings.MAX_POST_LENGTH:
-            raise serializers.ValidationError("This apupo is too long")
+            raise serializers.ValidationError("This apupo/post is too long")
         return value
--- apupos/serializers/apupo_serializer.py
+++ apupos/serializers/apupo_serializer.py
@@ -1,18 +1,31 @@
 from django.conf import settings
 from rest_framework import serializers
 
+from profiles.serializers.profile_serializer import ProfileSerializer
 from ..models.apupo import Apupo
 from ..serializers import ApupoCreateSerializer
+
+User = settings.AUTH_USER_MODEL
 
 
 class ApupoSerializer(serializers.ModelSerializer):
     likes = serializers.SerializerMethodField(read_only=True)
-    # content = serializers.SerializerMethodField(read_only=True)
     parent = ApupoCreateSerializer(read_only=True)
+    # user = UserSerializer()
+    # user = serializers.SerializerMethodField(read_only=True)
+    user = ProfileSerializer(source="user.profile", read_only=True)
 
     class Meta:
         model = Apupo
-        fields = ["id", "content", "likes", "is_retweet", "parent", "created_at"]
+        fields = [
+            "id",
+            "user",
+            "content",
+            "likes",
+            "is_retweet",
+            "parent",
+            "created_at",
+        ]
 
     def get_likes(self, obj):
         return obj.likes.count()
