02f55746b1398d75ae5cab72d0b0a1b71d5f67cd
Marlonn Locatelli <marlonn@gmail.com>
2025-08-14 14:32:12 -0300

    ✨ improved response to check auth endpoint (included user Profile data)

--- accounts/views/user_is_auth.py
+++ accounts/views/user_is_auth.py
@@ -2,8 +2,21 @@ from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.decorators import api_view, permission_classes
 
+from profiles.models.profile import Profile
+from profiles.serializers.profile_serializer import ProfileSerializer
+
 
 @api_view(["POST"])
 @permission_classes([IsAuthenticated])
-def is_authenticated(request):
-    return Response({"authenticated": True})
+def is_authenticated(request, *args, **kwargs):
+    user = request.user
+    queryset = Profile.objects.filter(user__username=user).first()
+
+    if not queryset:
+        return Response({"authenticated": "usuário não existe."}, status=404)
+
+    serializer = ProfileSerializer(queryset)
+
+    data = {"authenticated": True, "user": serializer.data}
+
+    return Response(data, status=200)
